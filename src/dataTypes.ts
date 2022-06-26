a60fc57f83031f1f332b40c3a0cf895c588b8850
ronami <ronena@wix.com>
2022-06-26 19:03:48 +0300

    wip

--- src/dataTypes.ts
+++ src/dataTypes.ts
@@ -1,14 +1,13 @@
-// Tokenizer types
-export type ParenToken<V> = { type: 'paren'; value: V };
+export type Paren<V> = { type: 'paren'; value: V };
 
-export type NumberToken<V> = { type: 'number'; value: V };
+export type Number<V> = { type: 'number'; value: V };
 
-export type StringToken<V> = { type: 'string'; value: V };
+export type String<V> = { type: 'string'; value: V };
 
-export type SymbolToken<V> = { type: 'symbol'; value: V };
+export type Symbol<V> = { type: 'symbol'; value: V };
 
-export type Token<V> =
-  | ParenToken<V>
-  | NumberToken<V>
-  | StringToken<V>
-  | SymbolToken<V>;
+// export type Token<V> =
+//   | ParenToken<V>
+//   | NumberToken<V>
+//   | StringToken<V>
+//   | SymbolToken<V>;
