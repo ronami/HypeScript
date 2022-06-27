b09a39b8ce92447fb3f653af24bbd95b97febaa0
Ronen Amiel <ronen.amiel@gmail.com>
2022-06-28 00:54:01 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { Tokenize } from './tokenize';
 import type { Parse } from './parse';
 
-type T = Tokenize<`const a = 5`>;
+type T = Tokenize<`const a = { hello: "world" }`>;
 type R = Parse<T>;
