ae5b1279fb97f81cf6b4402bb87639762304f06c
ronami <ronena@wix.com>
2022-06-28 20:48:17 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { Tokenize } from './tokenize';
 import type { Parse } from './parse';
 
-type T = Tokenize<`const a = { hello: "world" }`>;
+type T = Tokenize<`console.log(1)`>;
 type R = Parse<T>;
