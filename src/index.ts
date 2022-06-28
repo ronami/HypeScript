a5a1d0d2b464718e12097632e0fab49b00be9655
Ronen Amiel <ronen.amiel@gmail.com>
2022-06-28 22:44:33 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { Tokenize } from './tokenize';
 import type { Parse } from './parse';
 
-type T = Tokenize<`a.b.c`>;
+type T = Tokenize<`const a = console.log(1, 2, 3)`>;
 type R = Parse<T>;
