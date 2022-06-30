2ddbfa3d5574f9a656a6b7b9734ae3fc33ab6280
ronami <ronena@wix.com>
2022-06-30 13:22:05 +0300

    wip

--- src/parse.ts
+++ src/parse.ts
@@ -81,25 +81,18 @@ type ParseStatement<
   ? [ExpressionStatement<Cast<G, Array<any>>[0]>, Cast<G, Array<any>>[1]]
   : never;
 
-// type ParseExpressionHelperIf<G> = Cast<
-//   G,
-//   Array<any>
-// >[1][0] extends ParenToken<')'>
-//   ? Cast<G, Array<any>>[1][1] extends CurlyToken<'{'>
-//     ? Tail<Tail<Cast<G, Array<any>>[1]>>
-//     : never
-//   : never;
-
 type ParseIfStatement<T extends Array<Token<any>>> =
   T[0] extends ParenToken<'('>
     ? ParseExpression<Tail<T>> extends infer G
-      ? Cast<G, Array<any>>[1][0] extends ParenToken<')'>
-        ? Cast<G, Array<any>>[1][1] extends CurlyToken<'{'>
-          ? ParseFunctionBody<Tail<Tail<G[1]>>> extends infer B
-            ? [
-                IfStatement<Cast<G, Array<any>>[0], Cast<B, Array<any>>[0]>,
-                Cast<B, Array<any>>[1],
-              ]
+      ? Cast<G, Array<any>>[1] extends infer J
+        ? Cast<J, Array<any>>[0] extends ParenToken<')'>
+          ? Cast<J, Array<any>>[1] extends CurlyToken<'{'>
+            ? ParseFunctionBody<Tail<Tail<Cast<J, Array<any>>>>> extends infer B
+              ? [
+                  IfStatement<Cast<G, Array<any>>[0], Cast<B, Array<any>>[0]>,
+                  Cast<B, Array<any>>[1],
+                ]
+              : never
             : never
           : never
         : never
