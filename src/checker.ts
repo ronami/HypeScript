0c5fd0eb3483cb149f8975526f1bd52c019dba07
ronami <ronena@wix.com>
2022-06-30 16:59:20 +0300

    wip

--- src/checker.ts
+++ src/checker.ts
@@ -1,1 +1,37 @@
-export type Check<T> = T;
+import type {
+  BlockStatement,
+  BooleanLiteral,
+  ExpressionStatement,
+  NullLiteral,
+  NumericLiteral,
+  ReturnStatement,
+  StringLiteral,
+} from './ast';
+import type { Cast } from './utils/generalUtils';
+
+export type Check<T extends Array<any>> = CheckBlock<T>;
+
+type InferExpression<T> = T extends StringLiteral<any>
+  ? 'string'
+  : T extends NumericLiteral<any>
+  ? 'number'
+  : T extends NullLiteral
+  ? 'null'
+  : T extends BooleanLiteral<any>
+  ? 'boolean'
+  : 'unknown';
+
+type InferBlock<T extends Array<any>, S = {}> = T[0] extends ReturnStatement<
+  infer E
+>
+  ? InferExpression<E>
+  : [];
+
+type CheckBlock<
+  T extends Array<any>,
+  S = {},
+> = T[0] extends ExpressionStatement<infer E>
+  ? InferExpression<E>
+  : T[0] extends BlockStatement<infer B>
+  ? InferBlock<Cast<B, Array<any>>>
+  : [];
