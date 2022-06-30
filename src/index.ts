9c309105e877572b567bffb86bfc39d8f49fc94a
Ronen Amiel <ronen.amiel@gmail.com>
2022-06-30 13:22:05 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import type { Tokenize } from './tokenize';
 import type { Parse } from './parse';
 
-import type { Tail } from './utils/arrayUtils';
-import type { Cast } from './utils/generalUtils';
-
 type T = Tokenize<`
 if (foo) {
   log()
 }
 `>;
-type R = Parse<T>[0];
-
-// type Z = Cast<R, Array<any>>;
-// type A = Tail<Tail<Z[1]>>;
-// type B = R[0][1];
+type R = Parse<T>;
