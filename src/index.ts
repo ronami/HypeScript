c6b53cf342ce65c6d227c050fbfe7b842c8719bf
ronami <ronena@wix.com>
2022-06-30 15:34:04 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import type { Tokenize } from './tokenize';
 import type { Parse } from './parse';
+import type { Check } from './checker';
 
 type T = Tokenize<`
 const a: string = foo()
 `>;
 type R = Parse<T>;
+type C = Check<R>;
