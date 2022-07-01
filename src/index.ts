e29bb893b6ffbb206fafe908a8f3926124ce497c
ronami <ronena@wix.com>
2022-07-01 11:52:02 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,13 @@ import type { Tokenize } from './tokenize';
 import type { Parse } from './parse';
 import type { Check } from './checker';
 
-// type T = Tokenize<`
-// function ran (foo: string, bar: boolean) {}
-// `>;
-// type R = Parse<T>;
-
 type T = Tokenize<`
 function foo () {
-  const a = true
-
-  if (a) {
-    return a
+  function bar(a) {
+    return 1
   }
 
-  return "bar"
+  return bar
 }
 `>;
 type R = Parse<T>[0]['body'];
