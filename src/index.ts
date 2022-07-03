683741f816f5a7e7e4882da0879f744e74f7923e
ronami <ronena@wix.com>
2022-07-03 19:14:33 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import type { Tokenize } from './tokenizer';
-import type { Parse } from './parser';
-import type { Check } from './checker';
+// import type { Parse } from './parser';
+// import type { Check } from './checker';
 
-type T = Tokenize<`
-function foo () {
-  function bar(s: string, b: boolean) {
-    return 1
-  }
-
-  return bar("", true)
-}
-`>;
-type R = Parse<T>[0]['body'];
-type C = Check<[R]>;
+type T = Tokenize<`hello`>;
+// type R = Parse<T>[0]['body'];
+// type C = Check<[R]>;
