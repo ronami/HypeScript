076e8bcf46e6f2117035424ca910500a90ea65dd
Ronen Amiel <ronen.amiel@gmail.com>
2022-07-04 22:38:49 +0300

    wip

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -3,6 +3,7 @@ import type {
   EatFirstChar,
   FirstChar,
   ConcatStrings,
+  StringContains,
 } from './utils/stringUtils';
 import type { Cast, Numbers, Symbols } from './utils/generalUtils';
 import type {
@@ -18,85 +19,89 @@ import type {
   BracketToken,
   CommaToken,
 } from './tokens';
-import type { Error, SyntaxError } from './errors';
+import type { SyntaxError } from './errors';
 
-type TokenizeInput<I extends string> = FirstChar<I> extends infer F
-  ? EatFirstChar<I> extends infer E
-    ? F extends ' '
-      ? ['', E]
-      : F extends ','
-      ? [CommaToken, E]
-      : F extends '('
-      ? [ParenToken<'('>, E]
-      : F extends ')'
-      ? [ParenToken<')'>, E]
-      : F extends '['
-      ? [BracketToken<'['>, E]
-      : F extends ']'
-      ? [BracketToken<']'>, E]
-      : F extends '{'
-      ? [CurlyToken<'{'>, E]
-      : F extends '}'
-      ? [CurlyToken<'}'>, E]
-      : F extends '.'
-      ? [DotToken, E]
-      : F extends ';'
-      ? [SemicolonToken, E]
-      : F extends ':'
-      ? [ColonToken, E]
-      : F extends Numbers
-      ? TokenizeNumber<I, '', F>
-      : F extends '"'
-      ? TokenizeString<E, '"'>
-      : F extends "'"
-      ? TokenizeString<E, "'">
-      : F extends Symbols
-      ? TokenizeSymbol<I, '', F>
-      : SyntaxError<`Invalid character.`>
-    : never
-  : never;
+type TokenizeInput<
+  I extends string,
+  F extends string,
+  E extends string,
+  G extends boolean,
+> = F extends ','
+  ? [CommaToken, E]
+  : F extends '('
+  ? [ParenToken<'('>, E]
+  : F extends ')'
+  ? [ParenToken<')'>, E]
+  : F extends '['
+  ? [BracketToken<'['>, E]
+  : F extends ']'
+  ? [BracketToken<']'>, E]
+  : F extends '{'
+  ? [CurlyToken<'{'>, E]
+  : F extends '}'
+  ? [CurlyToken<'}'>, E]
+  : F extends '.'
+  ? [DotToken, E]
+  : F extends ';'
+  ? [SemicolonToken, E]
+  : F extends ':'
+  ? [ColonToken, E]
+  : F extends Numbers
+  ? TokenizeNumber<I, '', G, F>
+  : F extends '"'
+  ? TokenizeString<E, '"', G>
+  : F extends "'"
+  ? TokenizeString<E, "'", G>
+  : F extends Symbols
+  ? TokenizeSymbol<I, '', G, F>
+  : SyntaxError<`Invalid character.`>;
 
 type TokenizeNumber<
   I extends string,
-  A extends string = '',
+  A extends string,
+  G extends boolean,
   C extends string = FirstChar<I>,
 > = C extends Numbers
-  ? TokenizeNumber<EatFirstChar<I>, ConcatStrings<A, C>>
-  : [NumberToken<A>, I];
+  ? TokenizeNumber<EatFirstChar<I>, ConcatStrings<A, C>, G>
+  : [NumberToken<A, G>, I];
 
 type TokenizeString<
   I,
   W extends '"' | "'",
+  J extends boolean,
 > = I extends `${infer H}${W}${infer G}`
-  ? [StringToken<H>, G]
+  ? StringContains<H, '\n'> extends true
+    ? SyntaxError<'Unterminated string literal.'>
+    : [StringToken<H, J>, G]
   : SyntaxError<'Unterminated string literal.'>;
 
 type TokenizeSymbol<
   I extends string,
-  A extends string = '',
+  A extends string,
+  G extends boolean,
   C extends string = FirstChar<I>,
 > = C extends Symbols
-  ? TokenizeSymbol<EatFirstChar<I>, ConcatStrings<A, C>>
-  : [SymbolToken<A>, I];
+  ? TokenizeSymbol<EatFirstChar<I>, ConcatStrings<A, C>, G>
+  : [SymbolToken<A, G>, I];
 
 export type TokenizeSequence<
   I extends string,
   R extends Array<Token<any>> = [],
+  G extends boolean = false,
+  F extends string = FirstChar<I>,
+  E extends string = EatFirstChar<I>,
 > = I extends ''
   ? R
-  : TokenizeInput<I> extends infer P
-  ? P extends SyntaxError<any>
-    ? P
-    : TokenizeSequence<
-        Cast<P, Array<any>>[1],
-        Cast<P, Array<any>>[0] extends '' ? R : Push<R, Cast<P, Array<any>>[0]>
-      >
+  : F extends ' '
+  ? TokenizeSequence<E, R, G>
+  : F extends '\n'
+  ? TokenizeSequence<E, R, true>
+  : TokenizeInput<I, F, E, G> extends infer P
+  ? TokenizeHelper<P, R>
   : never;
 
-export type Tokenize<I extends string> = TokenizeSequence<I> extends infer G
-  ? G extends Array<any>
-    ? G
-    : G extends Error<any, any>
-    ? G
-    : never
-  : never;
+export type TokenizeHelper<P, R extends Array<any>> = P extends Array<any>
+  ? TokenizeSequence<P[1], Push<R, P[0]>>
+  : P;
+
+export type Tokenize<I extends string> = TokenizeSequence<I>;
--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -3,3 +3,13 @@ export type EatFirstChar<T> = T extends `${infer A}${infer B}` ? B : '';
 export type FirstChar<T> = T extends `${infer A}${infer B}` ? A : '';
 
 export type ConcatStrings<A extends string, B extends string> = `${A}${B}`;
+
+export type StringContains<I extends string, T extends string> = I extends T
+  ? true
+  : I extends `${T}${infer _}`
+  ? true
+  : I extends `${infer _0}${T}${infer _1}`
+  ? true
+  : I extends `${infer _}${T}`
+  ? true
+  : false;
