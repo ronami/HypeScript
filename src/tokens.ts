84a6d1ff573155336cc2690e53faf37fe311d318
ronami <ronena@wix.com>
2022-07-05 23:55:26 +0300

    wip

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,65 +1,65 @@
-export type TokenData = {
-  precedingLinebreak: boolean;
-  lineNumber: number;
+export type TokenData<P extends boolean, L extends number> = {
+  precedingLinebreak: P;
+  lineNumber: L;
 };
 
-export type ParenToken<V, D extends TokenData> = {
+export type ParenToken<V extends string, D extends TokenData<any, any>> = {
   type: 'paren';
   value: V;
   data: D;
 };
 
-export type BracketToken<V, D extends TokenData> = {
+export type BracketToken<V extends string, D extends TokenData<any, any>> = {
   type: 'bracket';
   value: V;
   data: D;
 };
 
-export type CurlyToken<V, D extends TokenData> = {
+export type CurlyToken<V extends string, D extends TokenData<any, any>> = {
   type: 'curly';
   value: V;
   data: D;
 };
 
-export type DotToken<D extends TokenData> = {
+export type DotToken<D extends TokenData<any, any>> = {
   type: 'dot';
   data: D;
 };
 
-export type SemicolonToken<D extends TokenData> = {
+export type SemicolonToken<D extends TokenData<any, any>> = {
   type: 'semicolon';
   data: D;
 };
 
-export type ColonToken<D extends TokenData> = {
+export type ColonToken<D extends TokenData<any, any>> = {
   type: 'colon';
   data: D;
 };
 
-export type NumberToken<V, D extends TokenData> = {
+export type NumberToken<V extends string, D extends TokenData<any, any>> = {
   type: 'number';
   value: V;
   data: D;
 };
 
-export type StringToken<V, D extends TokenData> = {
+export type StringToken<V extends string, D extends TokenData<any, any>> = {
   type: 'string';
   value: V;
   data: D;
 };
 
-export type SymbolToken<V, D extends TokenData> = {
+export type SymbolToken<V extends string, D extends TokenData<any, any>> = {
   type: 'symbol';
   value: V;
   data: D;
 };
 
-export type CommaToken<D extends TokenData> = {
+export type CommaToken<D extends TokenData<any, any>> = {
   type: 'comma';
   data: D;
 };
 
-export type Token<V, D extends TokenData> =
+export type Token<V extends string, D extends TokenData<any, any>> =
   | NumberToken<V, D>
   | BracketToken<V, D>
   | StringToken<V, D>
