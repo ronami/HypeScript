de48d326b7bf22abcbbe4b7e3d2b40b2e047e388
Ronen Amiel <ronen.amiel@gmail.com>
2022-07-08 15:56:24 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import type { Tokenize } from './tokenizer';
 import type { Parse } from './parser';
 // import type { Check } from './checker';
 
-type T = Tokenize<`function foo (a) {}`>;
+type T = Tokenize<`function foo () {} console.log()`>;
 type R = Parse<T>;
 // type C = Check<[R]>;
