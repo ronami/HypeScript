c2193881029a84a81ec0b02fabf03ab562d08f8e
ronami <ronena@wix.com>
2022-07-12 19:50:48 +0300

    wip

--- src/serializer.ts
+++ src/serializer.ts
@@ -30,7 +30,7 @@ export type Serialize<
     : H extends UnionType<infer U>
     ? SerializeUnion<U, N>
     : H extends ObjectType<infer O>
-    ? { [P in keyof O]: O[P] extends StaticType ? Serialize<O[P]> : never }
+    ? SerializeObject<O>
     : never
   : never;
 
@@ -40,13 +40,9 @@ type MapLiteralToType<T extends StaticType> = T extends NumberLiteralType<any>
   ? StringType
   : T extends BooleanLiteralType<any>
   ? BooleanType
-  : T extends ObjectType<infer O>
-  ? ObjectType<{
-      [P in keyof O]: O[P] extends StaticType ? MapLiteralToType<O[P]> : never;
-    }>
   : T;
 
-type SerializeArray<I extends StaticType> = Serialize<I, true> extends infer H
+type SerializeArray<I extends StaticType> = Serialize<I> extends infer H
   ? H extends string
     ? `${H}[]`
     : never
@@ -72,14 +68,20 @@ type SerializeUnion<
     : never
   : never;
 
-type R = Serialize<
-  ArrayType<
-    UnionType<
-      [
-        StringLiteralType<'hello'>,
-        NumberLiteralType<'2'>,
-        ArrayType<UnionType<[StringType, NumberType]>>,
-      ]
-    >
-  >
->;
+type SerializeObject<
+  U extends Array<[string, StaticType]>,
+  R extends string = '',
+> = U extends []
+  ? R extends ''
+    ? '{}'
+    : `{ ${R} }`
+  : U[0] extends [string, StaticType]
+  ? `${U[0][0]}: ${Serialize<U[0][1]>}` extends infer H
+    ? H extends string
+      ? SerializeObject<
+          Tail<U>,
+          U['length'] extends 1 ? `${R}${H};` : `${R}${H}; `
+        >
+      : never
+    : never
+  : never;
