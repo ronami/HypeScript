bd84815a343ea950a4ff13baa4421e910b39d074
ronami <ronena@wix.com>
2022-07-17 00:19:26 +0300

    wip

--- src/errors.ts
+++ src/errors.ts
@@ -1,24 +1,22 @@
-export type Error<
-  Type extends string,
-  Message extends string,
-  LineNumber extends number,
-> = {
-  type: Type;
+export type Error<Message extends string, LineNumber extends number> =
+  | SyntaxError<Message, LineNumber>
+  | ParsingError<Message, LineNumber>
+  | TypeError<Message, LineNumber>;
+
+export type SyntaxError<Message extends string, LineNumber extends number> = {
+  type: 'SyntaxError';
   message: Message;
   lineNumber: LineNumber;
 };
 
-export type SyntaxError<
-  Message extends string,
-  LineNumber extends number,
-> = Error<'SyntaxError', Message, LineNumber>;
+export type ParsingError<Message extends string, LineNumber extends number> = {
+  type: 'ParsingError';
+  message: Message;
+  lineNumber: LineNumber;
+};
 
-export type ParsingError<
-  Message extends string,
-  LineNumber extends number,
-> = Error<'ParsingError', Message, LineNumber>;
-
-export type TypeError<
-  Message extends string,
-  LineNumber extends number,
-> = Error<'TypeError', Message, LineNumber>;
+export type TypeError<Message extends string, LineNumber extends number> = {
+  type: 'TypeError';
+  message: Message;
+  lineNumber: LineNumber;
+};
