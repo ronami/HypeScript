e94d7ca13fd625cbfb5e5abb450d95af8ad9598c
Ronen Amiel <ronen.amiel@gmail.com>
2022-07-17 18:57:27 +0300

    wip

--- src/example.ts
+++ src/example.ts
@@ -1,19 +1,24 @@
-import type { TypeCheck } from '.';
+// import type { TypeCheck } from '.';
+import type { Tokenize } from './tokenizer';
+import type { Parse } from './parser';
+import type { Check } from './checker';
+// import type { Format } from './formatter';
+// import type { Error } from './errors';
+// import type { Token } from './tokens';
+// import type { BaseNode } from './ast';
 
-type Errors = TypeCheck<`
+type T = Tokenize<`
 
-function foo() {
-  if (a) {
-    return 'hello';
-  }
+function bar() {
+    if (a) {
+        return 2;
+    }
 
-  return 'world';
+    return 1;
 }
 
-const b = {
-    hello: 'hello'
-};
-
-const c = b[b.hello]
+const b: number = bar;
 
 `>;
+type P = Parse<T>;
+type C = Check<P>;
