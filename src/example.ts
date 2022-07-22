61499f156524357c0759c930e2f0a331681c475b
ronami <ronena@wix.com>
2022-07-22 23:46:04 +0300

    wip

--- src/example.ts
+++ src/example.ts
@@ -1,11 +1,6 @@
-// import type { TypeCheck } from '.';
 import type { Tokenize } from './Tokenizer';
 import type { Parse } from './Parser';
 import type { Check } from './Checker';
-// import type { Format } from './formatter';
-// import type { Error } from './errors';
-// import type { Token } from './tokens';
-// import type { BaseNode } from './ast';
 
 type T = Tokenize<`
 
@@ -13,4 +8,4 @@ let a: number;
 
 `>;
 type P = Parse<T>;
-// type C = Check<P>;
+type C = Check<P>;
