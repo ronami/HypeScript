324b5bd7ac4d6e10e02e7371a0206ca06d73e623
Ronen Amiel <ronen.amiel@gmail.com>
2022-07-22 23:37:36 +0300

    wip

---
+++ src/Checker/MatchType.ts
@@ -0,0 +1,75 @@
+import type {
+  AnyType,
+  BooleanLiteralType,
+  BooleanType,
+  NeverType,
+  NumberLiteralType,
+  NumberType,
+  StaticType,
+  StringLiteralType,
+  StringType,
+  UnionType,
+} from '.';
+import type { Tail } from '../Utils';
+
+export type MatchType<
+  TypeA extends StaticType,
+  TypeB extends StaticType,
+> = TypeA extends NeverType
+  ? false
+  : TypeB extends NeverType
+  ? false
+  : TypeA extends AnyType
+  ? true
+  : TypeB extends AnyType
+  ? true
+  : TypeA extends TypeB
+  ? TypeB extends TypeA
+    ? true
+    : false
+  : TypeA extends StringType
+  ? TypeB extends StringLiteralType<any>
+    ? true
+    : false
+  : TypeA extends BooleanType
+  ? TypeB extends BooleanLiteralType<any>
+    ? true
+    : false
+  : TypeA extends NumberType
+  ? TypeB extends NumberLiteralType<any>
+    ? true
+    : false
+  : TypeA extends UnionType<infer UnionTypesA>
+  ? TypeB extends UnionType<infer UnionTypesB>
+    ? UnionMatchUnion<UnionTypesA, UnionTypesB>
+    : TypeMatchUnion<UnionTypesA, TypeB>
+  : TypeB extends UnionType<infer UnionTypesB>
+  ? UnionMatchType<TypeA, UnionTypesB>
+  : false;
+
+type UnionMatchUnion<
+  UnionTypesA extends Array<StaticType>,
+  UnionTypesB extends Array<StaticType>,
+> = UnionTypesB extends []
+  ? true
+  : TypeMatchUnion<UnionTypesA, UnionTypesB[0]> extends true
+  ? UnionMatchUnion<UnionTypesA, Tail<UnionTypesB>>
+  : false;
+
+type TypeMatchUnion<
+  UnionTypes extends Array<StaticType>,
+  Type extends StaticType,
+> = UnionTypes extends []
+  ? false
+  : MatchType<UnionTypes[0], Type> extends true
+  ? true
+  : TypeMatchUnion<Tail<UnionTypes>, Type>;
+
+type UnionMatchType<
+  Type extends StaticType,
+  UnionTypes extends Array<StaticType>,
+> = UnionTypes extends []
+  ? true
+  : MatchType<Type, UnionTypes[0]> extends true
+  ? UnionMatchType<Type, Tail<UnionTypes>>
+  : false;
