f31eb33462c73e93dc63128918be08b3a123ff09
Ronen Amiel <ronen.amiel@gmail.com>
2022-07-25 01:24:09 +0300

    add tests

---
+++ src/Checker/Test/Comparison.test.ts
@@ -0,0 +1,34 @@
+import type { TypeCheck } from '../..';
+import { expectType } from '../../TestUtils';
+
+expectType<
+  TypeCheck<`
+
+const a: boolean = 1 == 2;
+
+`>
+>([
+  "3: This condition will always return 'false' since the types '1' and '2' have no overlap.",
+]);
+
+expectType<
+  TypeCheck<`
+
+const a: boolean = '1' == 2;
+
+`>
+>([
+  "3: This condition will always return 'false' since the types 'string' and 'number' have no overlap.",
+]);
+
+expectType<
+  TypeCheck<`
+
+let a = '1';
+
+let b = '2';
+
+const c: boolean = a === b;
+
+`>
+>([]);
---
+++ src/Checker/Test/IfStatement.test.ts
@@ -0,0 +1,24 @@
+import type { TypeCheck } from '../..';
+import { expectType } from '../../TestUtils';
+
+expectType<
+  TypeCheck<`
+
+if (a) {
+  b();
+}
+
+`>
+>(["3: Cannot find name 'a'.", "4: Cannot find name 'b'."]);
+
+expectType<
+  TypeCheck<`
+
+if (true) {
+  if (false) {
+    // empty
+  }
+}
+
+`>
+>([]);
