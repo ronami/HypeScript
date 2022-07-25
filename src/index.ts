e2120ad8f518d2c2d223853601cbe83bbd00779c
Ronen Amiel <ronen.amiel@gmail.com>
2022-07-26 01:44:37 +0300

    add a bit of comments to main export

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,20 @@ import type { Check } from './Checker';
 import type { Format, Error } from './Utils';
 
 export type TypeCheck<Input extends string> =
+  // Tokenize input
   Tokenize<Input> extends infer TokenList
     ? TokenList extends Error<any, any>
-      ? Format<[TokenList]>
+      ? // If any errors, format them
+        Format<[TokenList]>
       : TokenList extends Array<Token<any>>
-      ? Parse<TokenList> extends infer NodeList
+      ? // If successful, parse into an ast
+        Parse<TokenList> extends infer NodeList
         ? NodeList extends Error<any, any>
-          ? Format<[NodeList]>
+          ? // If any errors, format them
+            Format<[NodeList]>
           : NodeList extends Array<BaseNode<any>>
-          ? Format<Check<NodeList>>
+          ? // If successful, type-check and format errors
+            Format<Check<NodeList>>
           : never
         : never
       : never
